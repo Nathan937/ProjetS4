451a99ae9418b43f34a32a7cb92ed63f9bba6d5b
Dilege <33544167+Dilege@users.noreply.github.com>
2018-04-29 00:12:43 +0200

    Programmation
    Programme propre pour suivre les lignes

--- Programmes/Programme moteur/Infra_et_moteur/Infra_et_moteur.ino
+++ Programmes/Programme moteur/Infra_et_moteur/Infra_et_moteur.ino
@@ -1,4 +1,5 @@
 
+
 #include <DRV8835MotorShield.h>
 
 /*
@@ -10,8 +11,14 @@
 
  //Les pins du moteur 1 sont 7 et 9, moteur 2 sont 8 et 10.
 #define MODE_PIN 2
-#define LED_PIN 13
-#define SENS_LPIN 0
+#define LED_D 3
+#define LED_G 4
+#define LED_C 5
+#define BOUTON 12
+
+#define SENS_DPIN 0 //Droite
+#define SENS_GPIN 1 //Gauche
+#define SENS_CPIN 2 //Croisement
 
 //La veritable vitesse max(PWM) est 400.
 #define MAX_S 200
@@ -20,17 +27,26 @@
 
 DRV8835MotorShield motors;
 
-//resultat viteese moteur de la regulation avec capteur infra 
-int pid=0;
+//resultat vitesse moteur de la regulation avec capteur infra 
+int vitesse=100;
   int count=0;
+  int pid=0;
+  bool phase0=false;
+ bool phase1=false;
+ bool phase2=false;
+ bool phase3=false;
 
 void setup()
 {
 
   Serial.begin(9600);
   
-  pinMode(LED_PIN, OUTPUT);
-  
+  pinMode(LED_D, OUTPUT);
+   pinMode(LED_G, OUTPUT);
+pinMode(LED_C, OUTPUT);
+pinMode(BOUTON, INPUT);
+
+  //M2 est a gauche (correction de +25?); M1 est à droite
   
   // uncomment one or both of the following lines if your motors' directions need to be flipped
   //motors.flipM1(true);
@@ -38,11 +54,23 @@ void setup()
 
 
 // Ne pas oublier de brancher le mode sur le motor driver!!
+digitalWrite(BOUTON, HIGH);
   digitalWrite(MODE_PIN, HIGH);
 
 
-  
+// Le bouton doit etre appuyé pour demarrer le programme
+
+  do{
+  delay(1);
+  phase0=!digitalRead(BOUTON);
 }
+while(phase0==false);
+
+delay(1000);
+
+}
+
+
 
 void loop()
 {
@@ -51,84 +79,128 @@ void loop()
 //seuil suiveur de ligne blanc <100 noir>100
 do{
 
-  count++;
-  if(count>=500){
- Serial.println( analogRead(SENS_LPIN));
- count=0;
+  
+  
+if(100>analogRead(SENS_DPIN)){
+  digitalWrite(LED_D,HIGH);
+  motors.setM1Speed(vitesse);
+}
+if(100<analogRead(SENS_DPIN)){
+  digitalWrite(LED_D,LOW);
+  motors.setM1Speed(0);
+}
+
+if(100>analogRead(SENS_GPIN)){
+  digitalWrite(LED_G,HIGH);
+  motors.setM2Speed(vitesse);
+}
+if(100<analogRead(SENS_GPIN)){
+  digitalWrite(LED_G,LOW);
+  motors.setM2Speed(0);
+}
+
+if(100>analogRead(SENS_CPIN)){
+  digitalWrite(LED_C,HIGH);
+  
+  
+}
+if(100<analogRead(SENS_CPIN)){
+  digitalWrite(LED_C,LOW);
+ 
+}
+
+
+if(100<analogRead(SENS_GPIN)&&100<analogRead(SENS_DPIN)){
+  phase1=true;
+}
+ delay(1);
+}
+while(phase1==false);
+
+do{
+  motors.setM2Speed(vitesse);
+  motors.setM1Speed(vitesse);
+  
+  if(100>analogRead(SENS_CPIN)){
+
+  digitalWrite(LED_C,HIGH);
+ 
+}
+ if(100<analogRead(SENS_CPIN)){
+  phase2=true;
+  digitalWrite(LED_C,LOW);
+ 
+}
+
+}
+while(phase2==false);
+
+
+  
+  
+  motors.setM2Speed(vitesse);
+  motors.setM1Speed(-vitesse);
+  
+  delay(1000);
+ 
+
+do{
+  if(100>analogRead(SENS_GPIN)){
+ 
+  digitalWrite(LED_G,HIGH);
+  }
+  if(100<analogRead(SENS_GPIN)){
+ phase3=true;
+  digitalWrite(LED_G,LOW);
   }
   
- pid = REF_VIT - 0.25*(800 - analogRead(SENS_LPIN));
+  motors.setM2Speed(vitesse);
+  motors.setM1Speed(-vitesse);
  
- if(pid>=REF_VIT)pid = REF_VIT;
-if(100>analogRead(SENS_LPIN))digitalWrite(LED_PIN,HIGH);
-if(100<analogRead(SENS_LPIN))digitalWrite(LED_PIN,
-LOW);
+  delay(1);
+ }
+while(phase3==false);
+  
+  // run M1 motor with positive speed
+ do{
 
-motors.setM1Speed(pid);
-motors.setM2Speed(pid);
+  
+  
+if(100>analogRead(SENS_DPIN)){
+  digitalWrite(LED_D,HIGH);
+  motors.setM1Speed(vitesse);
+}
+if(100<analogRead(SENS_DPIN)){
+  digitalWrite(LED_D,LOW);
+  motors.setM1Speed(0);
+}
+
+if(100>analogRead(SENS_GPIN)){
+  digitalWrite(LED_G,HIGH);
+  motors.setM2Speed(vitesse);
+}
+if(100<analogRead(SENS_GPIN)){
+  digitalWrite(LED_G,LOW);
+  motors.setM2Speed(0);
+}
+
+if(100>analogRead(SENS_CPIN)){
+  digitalWrite(LED_C,HIGH);
+  
+  
+}
+if(100<analogRead(SENS_CPIN)){
+  digitalWrite(LED_C,LOW);
+ 
+}
+
+
+if(100<analogRead(SENS_GPIN)&&100<analogRead(SENS_DPIN)){
+  phase1=true;
+}
  delay(1);
 }
 while(1);
-
-  
-  // run M1 motor with positive speed
-  
-  digitalWrite(LED_PIN, HIGH);
-  
-  for (int speed = 0; speed <= MAX_S; speed++)
-  {
-    motors.setM1Speed(speed);
-    delay(4);
-  }
-
-  for (int speed = MAX_S; speed >= 0; speed--)
-  {
-    motors.setM1Speed(speed);
-    delay(4);
-  }
-  
-  // run M1 motor with negative speed
-  
-  digitalWrite(LED_PIN, LOW);
-  
-  for (int speed = 0; speed >= -MAX_S; speed--)
-  {
-    motors.setM1Speed(speed);
-    delay(4);
-  }
-  
-  for (int speed = -MAX_S; speed <= 0; speed++)
-  {
-    motors.setM1Speed(speed);
-    delay(4);
-  }
-
-//Sequences de 2 secondes à differentes vitesses
-
-
- for (int speed = 0; speed >= -MAX_S; speed=speed-50)
-  {
-    motors.setM1Speed(speed);
-    delay(2000);
-  }
-
-  for (int speed = -MAX_S; speed <= 0; speed++)
-  {
-    motors.setM1Speed(speed);
-    delay(4);
-  }
-  
-  for (int speed = 0; speed <= MAX_S; speed=speed+50)
-  {
-    motors.setM1Speed(speed);
-    delay(2000);
-  }
-
-  for (int speed = MAX_S; speed >= 0; speed--)
-  {
-    motors.setM1Speed(speed);
-    delay(4);
-  }
 
   
 }
